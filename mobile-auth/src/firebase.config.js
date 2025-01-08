24e8346b4dc1e382b1ce77e37f83ee24feb39c4c
unknown <nrpawar9756@gmail.com>
2025-01-08 12:57:44 +0530

    node module add

--- mobile-auth/src/firebase.config.js
+++ mobile-auth/src/firebase.config.js
@@ -6,17 +6,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env.REACT_APP_FIREBASE_APP_ID); 
 const firebaseConfig = {
-  apiKey: "",
+  apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "mobile-2d4aa.firebaseapp.com",
   projectId: "mobile-2d4aa",
   storageBucket: "mobile-2d4aa.firebasestorage.app",
   messagingSenderId: "982975176302",
-  appId: "",
-  measurementId: "G-44KW2GZNPF"
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "G-44KW2GZNPF",
 };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
